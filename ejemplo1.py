5e3409208b20b600de260e40a61698008d9248af
3qu1n12024 <kevylb@gmail.com>
2024-10-24 01:54:26 -0500

    Update ejemplo1.py
    Actualizacion 1

--- ejemplo1.py
+++ ejemplo1.py
@@ -6,23 +6,12 @@ Este es un archivo temporal.
 """
 
 import numpy as np
-import matplotlib.pyplot as plt
 import streamlit as st
 import datetime
 import pandas as pd
 
 st.title("Mi Primer Proyecto con streamlit")
 st.write("Hola _como_ estas")
-
-d = st.date_input('Fecha de cumpleaños', datetime.date(2019,7,6))
-st.write('Tu cumpleaños es:', d)
-
-option = st.selectbox('Como desearia ser contratado(a)?', ('Email','Telefono','Whatsapp', 'x'))
-st.write('Usted seleccionó la opcion:', option)
-
-n = st.slider("n", 5,100, step=1)
-chart_data = pd.DataFrame(np.random.randn(n),columns=['data'])
-st.line_chart(chart_data)
 
 df = pd.DataFrame(
 np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
